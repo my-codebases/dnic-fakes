a4d0db45f1063a5d591d6ee68e3e0b525adbf2a0
Gonzalo Barreiro <barreirogonzalo@outlook.com>
2024-02-27 02:52:47 +0000

    fix useres creation script

--- model/users.py
+++ model/users.py
@@ -25,11 +25,12 @@ def create_fake_user(shift_type):
     last_name = fake.last_name()
     email = first_name.lower()[:1] + last_name.lower() + '@gmail.com'
     phone = generate_fake_phone_number()
-    created_date = fake.date_between(start_date='-1y', end_date='now').strftime('%Y-%m-%d %H:%M:%S')
-    password = hashlib.sha256('Pass1234'.encode('utf-8')).hexdigest()
+    created_date = get_random_date(datetime(2023, 1, 31), datetime(2024, 1, 31), random.random()).strftime('%Y-%m-%d %H:%M:%S')
+    password = hashlib.sha256('password'.encode('utf-8')).hexdigest()
     material_number = ''.join(random.choices('ABCDEFGHIJKLMNOPQRSTUVWXYZ0123456789', k=6))
-    id_card_expiration_date = get_random_date(id_card_expiration_start, id_card_expiration_end, random.random())
-    passport_expiration_date = get_random_date(passport_expiration_start, passport_expiration_end, random.random())
+    id_card_expiration_date = fake.date_between(start_date=id_card_expiration_start, end_date=id_card_expiration_end)
+    passport_expiration_date = fake.date_between(start_date=passport_expiration_start, end_date=passport_expiration_end)
+
     if shift_type == 'PASSPORT':
         id_card_expiration_date += relativedelta(years = random.randint(2, 9))
 
@@ -43,9 +44,10 @@ def create_fake_user(shift_type):
         'last_updated_date': created_date,
         'status': 'ACTIVE',
         'password': password,
+        'role': 'USER',
         'material_number': material_number,
         'id_card_expiration_date': id_card_expiration_date.strftime('%Y-%m-%d %H:%M:%S'),
-        'passport_expiration_date': '' if shift_type == 'PASSPORT' else passport_expiration_date.strftime('%Y-%m-%d %H:%M:%S')
+        'passport_expiration_date': None if shift_type == 'PASSPORT' else passport_expiration_date.strftime('%Y-%m-%d %H:%M:%S')
     }
     
     return user
@@ -55,7 +57,6 @@ def generate_fake_phone_number():
     company_code = '9' + str(random.randint(1,9))
     digits = random.choices('0123456789', k=6)
     return f"{country_code} {company_code} {''.join(digits[:3])} {''.join(digits[3:])}"
-
 
 def get_random_date(start, end, value):
     offset = math.floor(value * (end - start).total_seconds())
@@ -69,6 +70,6 @@ def add_check_digit(document_number):
     factors = [2, 9, 8, 7, 6, 3, 4]
     check_digit = 0
     for i in range(7):
-        check_digit += (document_number // 10 ** (6-i)) % 10
+        check_digit += ((document_number // 10 ** (6-i)) % 10) * factors[i]
     check_digit = 10 - check_digit % 10
     return document_number * 10 + check_digit
